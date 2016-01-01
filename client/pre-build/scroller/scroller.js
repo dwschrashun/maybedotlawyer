d5ef694a6f19d7a885ee2d443fbb4ab50d171199
dwschrashun <dwschrashun@gmail.com>
2016-01-01 15:10:42 -0500

    attempt at loading screen

--- client/pre-build/scroller/scroller.js
+++ client/pre-build/scroller/scroller.js
@@ -6,11 +6,21 @@ app.config(function($stateProvider) {
     });
 });
 
-app.controller("ScrollerController", function ($scope, LyricsFactory) {
+app.controller("ScrollerController", function ($scope, LyricsFactory, $window) {
 	$scope.lyricDisplay = false;
 	$scope.scrollForever = false;
 	$scope.player = false;
+	$scope.loaded = false;
 	$scope.lyrics = LyricsFactory;
+	// $scope.$on('$viewContentLoaded', function(event){ 
+	// 	console.log("loaded", event);
+	// });
+	// console.log("window?", $window);
+	angular.element(window).on("load", function (event) {
+		console.log("loaded");
+		$scope.loaded = true;
+	});
+	// console.log("onload?", $window.onload);
 	//$scope.panoStyle = "height: 0px; width: 0px";
 	//$scope.panoStyle = "overflow: hidden; height: 0px; width: 0px";
 	$scope.showLyrics = function () {
